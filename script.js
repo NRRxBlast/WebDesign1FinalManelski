db7e19bf33bd829572fad94796250fe91f218545
Glitch (glitch-hello-website) <none>
2023-10-14 09:22:39 +0000

    👘👩‍❤️‍💋‍👩 Checkpoint ./script.js:60970214/617 ./index.html:60970214/273 ./faq.html:60970214/1

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+let getClick, setClick
+
 function homePage(){
   window.location.href="index.html";
 }
 function contentPage(){
+  
   window.location.href="content.html";
+  getClick = 
+  
 }
 function faqPage(){
   window.location.href="faq.html";
+}
+function changeDisplay(){
+  window.getElementById("contentColorsPatterns").style.display = "none";
 }
