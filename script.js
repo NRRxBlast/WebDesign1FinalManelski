6de6c2d6592e474dedc7f375c48abdd92dd43b52
Glitch (glitch-hello-website) <none>
2023-10-14 09:46:40 +0000

    👓👜 Checkpoint ./script.js:60970214/2132 ./content.html:60970214/25 ./index.html:60970214/316

--- script.js
+++ script.js
@@ -6,12 +6,52 @@ function homePage(){
 function contentPage(){
   
   window.location.href="content.html";
-  changeDisplay();
+  
   
 }
 function faqPage(){
   window.location.href="faq.html";
 }
-function changeDisplay(){
-  document.getElementById("contentColorsPatterns").style.display = "none";
+function setDisplay(disp){
+  if(disp == 1){
+    setClick = localStorage.setItem("value", 1);
+    
+  }
+  else if(disp == 2){
+    setClick = localStorage.setItem("value", 2);
+  }
+  else if(disp == 3){
+    setClick = localStorage.setItem("value", 3);
+  }
+  else if(disp == 4){
+    setClick = localStorage.setItem("value", 4);
+  }
+}
+function getDisplay(){
+  getClick = localStorage.getItem("value");
+  if(getClick == 1){
+    document.getElementById("contentColorsPatterns").style.display = "flex";
+    document.getElementById("contentTypography").style.display = "none";
+    document.getElementById("contentButtons").style.display = "none";
+    document.getElementById("contentLayout").style.display = "none";
+    
+  }
+  else if(getClick == 2){
+    document.getElementById("contentColorsPatterns").style.display = "none";
+    document.getElementById("contentTypography").style.display = "grid";
+    document.getElementById("contentButtons").style.display = "none";
+    document.getElementById("contentLayout").style.display = "none";
+  }
+  else if(getClick == 3){
+    document.getElementById("contentColorsPatterns").style.display = "none";
+    document.getElementById("contentTypography").style.display = "none";
+    document.getElementById("contentButtons").style.display = "flex";
+    document.getElementById("contentLayout").style.display = "none";
+  }
+  else if(getClick == 4){
+    document.getElementById("contentColorsPatterns").style.display = "none";
+    document.getElementById("contentTypography").style.display = "none";
+    document.getElementById("contentButtons").style.display = "none";
+    document.getElementById("contentLayout").style.display = "block";
+  }
 }
